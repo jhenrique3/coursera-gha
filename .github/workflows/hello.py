8487a2c69666b24239336f9f5ae51def8c48d9b2
jhenrique3 <65829630+jhenrique3@users.noreply.github.com>
2025-07-04 19:06:27 +0000

    Adding vulnerable file

--- .github/workflows/hello.py
+++ .github/workflows/hello.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import subprocess
+import sys
+
+# Vulnerable
+user_input = "foo && cat /etc/passwd" # value supplied by user
+subprocess.call("grep -R {} .".format(user_input), shell=True)
+
+# Vulnerable
+user_input = "cat /etc/passwd" # value supplied by user
+subprocess.run(["bash", "-c", user_input], shell=True)
+
+# Not vulnerable
+user_input = "cat /etc/passwd" # value supplied by user
+subprocess.Popen(['ls', '-l', user_input])
+
+# Not vulnerable
+subprocess.check_output('ls -l dir/')
